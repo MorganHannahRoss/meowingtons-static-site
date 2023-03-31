bf0f4e2b39ed7ce60b0e2825651a11b3d799c7b4
Morgan Ross <morgan.ross@developersinstitute.co.nz>
2023-03-31 22:40:26 +1300

    CSS and button for checkout added

--- src/App.js
+++ src/App.js
@@ -1,48 +1,153 @@
+import { checkout } from "./services/checkout";
 import { React, useEffect, useState } from "react";
 import "./App.css";
+import Select from "react-select";
 
 // The function that makes the fetch request to the Products API
 import { getProducts } from "./services/getProducts";
+import { check } from "prettier";
+
+//filter options from react-select
+const filterOptions = [
+  { value: "all", label: "All" },
+  { value: "beds", label: "Cat Beds and Cushions" },
+  { value: "Trees", label: "Cat Trees and Condos" },
+  { value: "treats", label: "Cat Toys and Treats" },
+];
+//sort options for react select
+const sortOptions = [
+  { value: "low", label: "Price: low" },
+  { value: "high", label: "Price: high" },
+];
 
 function App() {
   // use the products variable to read all of your products
   // and display them on your page
-  const [products, setProducts] = useState([]);
 
+  //filter by dropdown menu (beds/Trees/treats)
+  const [products, setProducts] = useState([]);
+  const [allProducts, setAllProducts] = useState([]);
+  const [selectedCategory, setSelectedCategory] = useState(undefined);
+
+  //sort by dropdown menu (price /old etc)
+  const [selectedSort, setSelectedSort] = useState(undefined);
+  //userface to change a category
+  function handleCategoryChange(option) {
+    console.log(option.value);
+    setSelectedCategory(option);
+    if (option.value === "all") {
+      return setProducts(allProducts);
+    }
+    const filtered = allProducts.filter((product) => {
+      const isCategory = product.metadata.category === option.value;
+      return isCategory;
+    });
+    setProducts(filtered);
+  }
+  //filter the products
+  function handleSortChange(option) {
+    console.log(option.value);
+    setSelectedSort(option);
+    const sorted = products.sort((a, b) => {
+      if (option.value === "low") {
+        return a.prices[0].unit_amount - b.prices[0].unit_amount;
+      }
+      if (option.value === "high") {
+        return b.prices[0].unit_amount - a.prices[0].unit_amount;
+      }
+    });
+    setProducts(sorted);
+  }
+
+  //download products
   useEffect(() => {
     const loadData = async () => {
       const products = await getProducts();
       setProducts(products);
+      setAllProducts(products);
     };
 
     loadData();
   }, []);
 
-  console.log(products);
+  //sort by - price - low to high/high to low
 
-  //TEST
+  //Featured products menu
+  console.log(products);
+  const featuredProducts = allProducts.filter((product) => {
+    const isFeatured = product.metadata.featured === "yes";
+    return isFeatured;
+  });
+
+  console.log(featuredProducts);
+
+  // HTML BELOW
+
   return (
-    <div className="container">
-      <h1>Meowingtons</h1>
-      {products.map((product, index) => (
-        <StoreProduct key={index} product={product} />
-      ))}
+    <div className="body">
+      <header>
+        <img
+          class="logo"
+          alt="Meowingtons cat logo"
+          src="/logo-meowingtons.webp"
+        />
+      </header>
+
+      <section>
+        <h1>Featured Products</h1>
+
+        <div class="feature-grid">
+          {featuredProducts.map((product, index) => (
+            <StoreProduct key={index} product={product} />
+          ))}
+        </div>
+      </section>
+
+      <section>
+        <nav class="sort-by">
+          <span>
+            Filter By
+            <Select
+              options={filterOptions}
+              value={selectedCategory}
+              onChange={handleCategoryChange}
+            />
+          </span>
+          <span>
+            Sort By
+            <Select
+              options={sortOptions}
+              value={selectedSort}
+              onChange={handleSortChange}
+            />
+          </span>
+        </nav>
+
+        <main class="container">
+          {products.map((product, index) => (
+            <StoreProduct key={index} product={product} />
+          ))}
+        </main>
+      </section>
     </div>
   );
 }
-// //Practice - TEST PRODUCTS BELOW
+// products below
 
 const StoreProduct = ({ product }) => {
+  function handleClick() {
+    checkout(product.prices[0].id);
+  }
   return (
-    <li>
+    <section className="product-card">
       <p>{product.name}</p>
       <p>
-        <img src={product.images[0]} alt={product.name} width="50%" />
+        <img src={product.images[0]} alt={product.name} width="100%" />
       </p>
-      <p> {product.price}</p>
+      <p>${product.prices[0].unit_amount / 100}.00</p>
       <p>{product.description}</p>
-      <button>Buy Now</button>
-    </li>
+      <button onClick={handleClick}>Buy Now</button>
+    </section>
   );
 };
 
--- wireframes/wireframes.md
+++ wireframes/wireframes.md
@@ -1,1 +1,3 @@
-https://excalidraw.com/%20new%20project#json=xAeqGnRknHZ-wBobkyuO_,Ccnt3Kd6skgUfEVGQXWUqA
+## Wireframes
+
+<img src="./wireframes/Wireframes online shop .png">
